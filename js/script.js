93d700f1ce1713ceece80fa5e1838317c203d34c
Sahil <hanss9545@gmail.com>
2021-09-28 18:05:40 +0530

    page finished

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let rightPart=document.querySelector(".right-part");
 let cross=document.querySelector(".cross");
 let scrollTop=document.querySelector(".scroll-top");
 let personeNames=["Kenneth Ahn 1","Kenneth Ahn 2","Kenneth Ahn 3","Kenneth Ahn 4",]
-let personName=document.querySelector(".right-part h1");
+let personName=document.querySelector(".right-part h3");
 let persondetailspara1=document.querySelector(".right-part #para1");
 let persondetailspara2=document.querySelector(".right-part #para2");
 
@@ -13,10 +13,10 @@ let Para1=["1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
 "4 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt itaque odit tempore eum similique consequuntur amet autem accusamus impedit vel eaque, eveniet tenetur ullam aspernatur iusto quaerat sed architecto corrupti, facilis commodi quam adipisci non rem! Ullam aperiam neque voluptates consequuntur molestiae, earum quia tempore similique tenetur quod adipisci a."];
 
 
-let Para2=["1Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet qui? Laborum amet doloribus nemo libero placeat temporibus rerum dolor minima nisi eius.",
-"2Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet qui? Laborum amet doloribus nemo libero placeat temporibus rerum dolor minima nisi eius.",
-"3Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet qui? Laborum amet doloribus nemo libero placeat temporibus rerum dolor minima nisi eius.",
-"4Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet qui? Laborum amet doloribus nemo libero placeat temporibus rerum dolor minima nisi eius."];
+let Para2=["1Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveni",
+"2Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet qu",
+"3Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet q.",
+"4Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dignissimos quasi eius, voluptas sed provident beatae eveniet q"];
 
 
 for(let i=0;i<4;i++){
@@ -24,7 +24,7 @@ for(let i=0;i<4;i++){
 
         personName.textContent=personeNames[i];
         persondetailspara1.textContent=Para1[i];
-        persondetailspara2.textContent=Para1[i];
+        persondetailspara2.textContent=Para2[i];
 
 
 
